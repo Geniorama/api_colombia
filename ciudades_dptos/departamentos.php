e76a5efa82d2cd57c97d71d9825a62523df6d362
Geniorama <geniorama.info@gmail.com>
2019-11-12 15:22:38 -0500

    first commit

---
+++ ciudades_dptos/departamentos.php
@@ -0,0 +1,48 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
+include "config.php";
+include "utils.php";
+
+
+$dbConn =  connect($db);
+
+/*
+  listar todos los posts o solo uno
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * FROM departamentos where id_departamento=:id");
+      $sql->bindValue(':id', $_GET['id']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetchAll(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+    else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM departamentos");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	  }
+}
+
+
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
+
+?>
---
+++ ciudades_dptos/index.php
@@ -0,0 +1,11 @@
+<?php
+
+header("HTTP/1.1 200 OK");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
